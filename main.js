2ae9481d3cb0c21692b041776b9fdbb4baacd171
Diomar <diomarpr@gmail.com>
2022-05-24 05:52:37 -0300

    teste

--- main.js
+++ main.js
@@ -2,28 +2,28 @@ const modal = document.querySelector('.modal__container');
 
 
 
-// function addEvent(obj, evt, fn) {
-//   if (obj.addEventListener) {
-//     obj.addEventListener(evt, fn, false);
-//   }
-//   else if (obj.attachEvent) {
-//     obj.attachEvent("on" + evt, fn);
-//   }
-// }
-// addEvent(window, "load", function (e) {
-//   addEvent(document, "mouseout", function (e) {
-//     e = e ? e : window.event;
-//     var from = e.relatedTarget || e.toElement;
-//     if (!from || from.nodeName == "HTML") {
-//       // Aqui você coloca o seu popup
-//       // modal.classList.add('active');
-//     }
-//   });
-// });
+function addEvent(obj, evt, fn) {
+  if (obj.addEventListener) {
+    obj.addEventListener(evt, fn, false);
+  }
+  else if (obj.attachEvent) {
+    obj.attachEvent("on" + evt, fn);
+  }
+}
+addEvent(window, "load", function (e) {
+  addEvent(document, "mouseout", function (e) {
+    e = e ? e : window.event;
+    var from = e.relatedTarget || e.toElement;
+    if (!from || from.nodeName == "HTML") {
+      // Aqui você coloca o seu popup
+      modal.classList.add('active');
+    }
+  });
+});
 
-// function closeModal() {
-//   modal.classList.remove('active')
-// }
+function closeModal() {
+  modal.classList.remove('active')
+}
 
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
